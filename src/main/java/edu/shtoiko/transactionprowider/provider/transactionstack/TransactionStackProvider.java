ba11cd2b684ab31fafbd142ba37ef99f4ec9c04d
Vasyl Shtoiko <vasylshtoiko@Vasyls-MacBook-Pro.local>
2024-07-27 17:02:42 +0200

    Add dependencies in pom.xml

--- src/main/java/edu/shtoiko/transactionprowider/provider/transactionstack/TransactionStackProvider.java
+++
@@ -1,9 +0,0 @@
-package edu.shtoiko.transactionprowider.provider.transactionstack;
-
-import edu.shtoiko.transactionprowider.model.entity.Transaction;
-import reactor.core.publisher.Flux;
-
-public interface TransactionStackProvider {
-    Flux<Transaction> getTransactionQueue();
-    void removeFromQueue(Transaction transaction);
-}
